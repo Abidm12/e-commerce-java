d2bceae9b6680a91b3372c700c7433fed5dae37e
Abidm12 <119511185+Abidm12@users.noreply.github.com>
2026-02-22 10:42:20 +0530

    Update DBConnection.java

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
         private static final String URL = "jdbc:postgresql://localhost:5432/ecommerce_db";
-        private static final String USER = "postgres";
-        private static final String PASSWORD = "password";
+        private static final String USER = "your_username";
+        private static final String PASSWORD = "your_password";
 
         public static Connection getConnection() throws SQLException {
             return DriverManager.getConnection(URL, USER, PASSWORD);
